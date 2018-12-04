a534023a352fca095bdae78841408e7b1a528168
Subject-Infinite <hkqureshi@live.co.uk>
2018-12-04 23:08:53 +0000

    created bash script to detect most frequent days set to git log, though git log must be manually updated first. May need to check if spelling of thursday is correct in array

---
+++ date_freq.sh
@@ -0,0 +1,17 @@
+echo "insert file name of git log here"
+read response
+a1=Mon
+a2=Tue
+a3=Wed
+a4=Thurs
+a5=Fri
+a6=Sat
+a7=Sun
+
+days=(Mon Tue Wed Thurs Fri Sat Sun)
+
+for i in ${days[*]}
+do
+	freq=$(egrep $i $response | wc -l)
+	echo "$i appears $freq times"
+done
